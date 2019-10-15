d12b0076a2fbe9a4de9accaa0f424e14f488095a
Hugo Delile <hugo.delile@outlook.com>
2019-10-15 13:53:35 +0200

    Correction

--- othello.pl
+++ othello.pl
@@ -1,19 +1,21 @@
-play(Game) <- 
+initialize(Game, Position, Player).
+
+play(Game) :- 
 	initialize(Game, Position, Player),
 	display(Position, Player),
 	play(Position, Player, Result).
 	
-play(Position, Player, Result) <- 
+play(Position, Player, Result) :- 
 	game_over(Position, Player, Result), !, announce(Result).
 
-play(Position, Player, Result) <-
+play(Position, Player, Result) :-
 	choose_move(Position, Player, Move),
 	move(Move, Position, Position1),
-	display-game(Position1, Player),
+	display_game(Position1, Player),
 	next_player(Player, Player1),
-	!, play(Position1, Player1, Result).
+	!, play(Position1, Player1, Result).% S'il y a un moyen de jouer alors on joue
 	
-evaluate_and_choose([Move|Moves], Position, Record, BestMove) <-
+evaluate_and_choose([Move|Moves], Position, Record, BestMove) :-
 	% choisir le BestMove dans l'ensemble des Moves à partir de la position courante ; 
 	% Record enregistre le meilleur mouvement courant
 	move(Move, Position, Position1),
@@ -23,13 +25,13 @@ evaluate_and_choose([Move|Moves], Position, Record, BestMove) <-
 	
 evaluate_and_choose([], Position, (Move, Value), Move).
 
-update(Move, Value, (Move1, Value1), (Move1,Value1)) <-
-	Value <= Value1.
+update(Move, Value, (Move1, Value1), (Move1,Value1)) :-
+	Value := Value1.
 	
-update(Move, Value, (Move1,Value1), (Move,Value)) <-
+update(Move, Value, (Move1,Value1), (Move,Value)) :-
 	Value > Value1.
 	
-choose_move(Position, Computer, Move) <-
+choose_move(Position, Computer, Move) :-
 	set_of(M, move(Position, M), Moves),
 	evaluate_and_choose(Moves, Position, (nil, -1000), Move).
 	
@@ -42,23 +44,23 @@ choose_move(Position, Computer, Move) <-
 %	anticipant de Depth strates Flag, indique si l'on minimise ou si l'on maximise couramment.
 %	Record enregistre le meilleur mouvement courant
 
-evaluate_and_choose([Move|Moves], Position, D, MaxMin, Record, Best) <- 
+evaluate_and_choose([Move|Moves], Position, D, MaxMin, Record, Best) :- 
 	move(Move, Position, Position1),
-	minimax(D, Position1, MaxMin, MoveX, Value).
+	minimax(D, Position1, MaxMin, MoveX, Value),
 	update(Move, Value, Record, Record1),
 	evaluate_and_choose(Moves, Position, D, MaxMin, Record1, Best).
 	
 evaluate_and_choose([], Position, D, MaxMin, Record, Record).
 
-minimax(0, Position, MaxMin, Move, Value) <-
+minimax(0, Position, MaxMin, Move, Value) :-
 	value(Position, V),
 	Value := V * MaxMin.
 	
-minimax(D, Position, MaxMin, Move, Value) <-
+minimax(D, Position, MaxMin, Move, Value) :-
 	D > 0,
 	set_of(M, move(Position, M), Moves),
-	D1 := D - 1
-	MinMax := - MaxMin,
+	D1 := D - 1,
+	MinMax := -MaxMin,
 	evaluate_and_choose(Moves, Position, D1, MinMax, (nil, -1000), (Move, Value)).
 	
 %update(Move, Value, Record, Record1)
