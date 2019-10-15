9f732aa6e1265f2d0e96b9e3cffd99d6058d5a7f
Balthazar Frolin <balthov60@gmail.com>
2019-10-15 17:46:31 +0200

    add primitives

--- main.pl
+++ main.pl
@@ -1,12 +1,28 @@
-main :- 
-	Othelier=[v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,b,n,v,v,v,v,v,v,n,b,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v],
-	writeln('Bienvenue sur Prothello'),
-	display(Othelier).
+% Primitive gestion board
 
+% Met dans RESULT la valeur LIST[X][Y]
+case(X, Y, LIST, RESULT) :-
+    Z is X * 8 + Y,
+    nth0(Z, LIST, RESULT).
 
-display([]).
-display([H1,H2,H3,H4,H5,H6,H7,H8|Matrice]) :-
-	format('~w ~w ~w ~w ~w ~w ~w ~w~n', [H1,H2,H3,H4,H5,H6,H7,H8]), display(Matrice).
+% Remplace ma valeur LIST[X][Y] par NEW et renvoi RESULT_LIST la nouvelle LIST
+replace(X, Y, NEW, LIST, RESULT_LIST) :-
+    Z is X * 8 + Y,
+    nth0(Z, LIST, _, TEMP),
+    nth0(Z, RESULT_LIST, NEW, TEMP).
 
+% Print board
+print_matrice([]).
+print_matrice([H1,H2,H3,H4,H5,H6,H7,H8|Matrice]) :-
+    format('~w ~w ~w ~w ~w ~w ~w ~w~n', [H1,H2,H3,H4,H5,H6,H7,H8]),
+    print_matrice(Matrice).
 
+% Init board
+init_board(MAT) :-
+    MAT = [v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, v, b, n, v, v, v, v, v, v, n, b, v, v, v, v, v, v, v, v, v, v, v,v, v, v, v, v, v, v, v,v, v, v, v, v, v, v, v].
 
+main :-
+    init_board(MAT),
+    print_matrice(MAT),
+    replace(0, 0, b, MAT, RESULT_MAT),
+    print_matrice(RESULT_MAT).
