07a129a26e1edb2acdbac0a92d81fb30544bf9b2
TRAN Duy Nghia <tdnghia98@gmail.com>
2019-10-18 14:30:28 +0200

    Update game.pl

--- game.pl
+++ game.pl
@@ -2,7 +2,10 @@ case(X, Y, LIST, RESULT) :-
     Z is X * 8 + Y,
     nth0(Z , LIST, RESULT).
  
- 
+ replace(X, Y, N, L, R) :-
+    Z is X * 8 + Y,
+    nth0(Z, L, _, RR),
+    nth0(Z, R, N, RR).
 
 % placer un pion. Return false si autre statut que vide 
 % X,Y => Coordonnée, LIST => plateau, COULEUR => n(noir) ou b(blanc) en fonction du joueur
@@ -12,3 +15,9 @@ placer(X, Y, LIST, COULEUR, RESULT):-
             replace(X, Y, COULEUR, LIST, RESULT)
     ; fail).
 
+
+flipPion(Plateau, X, Y, R):-
+   case(X, Y, Plateau, Z),
+   (Z = 'n' -> replace(X, Y, 'b', Plateau, R);
+   Z = 'b' -> replace(X, Y, 'n', Plateau, R);
+   fail).
