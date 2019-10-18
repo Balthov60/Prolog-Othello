6649ef59f69509baf5e9f25992d9528186c9221b
Dante-DaCapo <nathan-savina@hotmail.fr>
2019-10-18 17:30:26 +0200

    Lecture X Y pour coup joueur

--- game.pl
+++ game.pl
@@ -24,15 +24,29 @@ flipPion(Plateau, X, Y, R):-
 
 listeNonVide([_|_]).
 
+/*
 % lit les coordonnées du coup du joueur
 % TO DO : verif la possibilité du coup
-lire_coord_joueur(X, Y):-
-	writeln('Entrer coord X puis Y (0 <= X,Y <= 7,) :'),
+lire_coord_joueur(LISTE_COUP_POSSIBLE, X, Y):-
+	writeln('Entrer coord X puis Y (0 <= X Y <= 7,) :'),
 	read(X),
 	read(Y),
-	(X > 7 -> lire_coord_joueur(X,Y);
-	 X < 0 -> lire_coord_joueur(X,Y);
-	 Y > 7 -> lire_coord_joueur(X,Y);
-	 Y < 0 -> lire_coord_joueur(X,Y);
+	writeln(X),
+	writeln(Y),
+	(X > 7 -> lire_coord_joueur(LISTE_COUP_POSSIBLE,_,_);
+	 X < 0 -> lire_coord_joueur(LISTE_COUP_POSSIBLE,_,_);
+	 Y > 7 -> lire_coord_joueur(LISTE_COUP_POSSIBLE,_,_);
+	 Y < 0 -> lire_coord_joueur(LISTE_COUP_POSSIBLE,_,_);
+	 not(member([X,Y], LISTE_COUP_POSSIBLE)) ->  writeln('Ce coup n\'est pas permis !'),
+ 						lire_coord_joueur(LISTE_COUP_POSSIBLE, _, _);
 	 !
  	 ).
+
+ testMember(X,Y):-
+	 member([X,Y],[[1,2],[3,4]]).
+ */
+
+lire_coord_joueur(X, Y):-
+	writeln('Entrer coord X puis Y (0 <= X Y <= 7,) :'),
+	read(X),
+	read(Y).
