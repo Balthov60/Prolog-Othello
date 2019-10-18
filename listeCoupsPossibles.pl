eeb22ccaef5bd0552ec5a22d80000a94d7aa6e8f
cmenseau <cyrielle.menseau@insa-lyon.fr>
2019-10-18 17:19:43 +0200

    liste des coups possibles

---
+++ listeCoupsPossibles.pl
@@ -0,0 +1,50 @@
+
+coupPossible(Plateau,X,Y,Couleur):- 
+	estVide(Plateau,X,Y), 
+	aVoisinCouleurOpposee(Plateau,X,Y,Couleur).
+%	encadreCouleurOpposee(Plateau,X,Y,Couleur,X2,Y2).
+
+case(X, Y, LIST, RESULT) :-
+    Z is X * 8 + Y,
+    nth0(Z, LIST, RESULT).
+
+% est vrai si la case est vide
+estVide(Plateau,X,Y):- case(X,Y,Plateau,v).
+estNonVide(Plateau,X,Y):- case(X,Y,Plateau,R), R\==v.
+
+% vrai si une case avec un pion noir a au moins un voisin blanc
+aVoisinCouleurOpposee(Plateau,X,Y,n):- coordEnHaut(X,Y,XNew,YNew), case(XNew,YNew,Plateau,b). % au dessus
+aVoisinCouleurOpposee(Plateau,X,Y,n):- coordEnHautDroite(X,Y,XNew,YNew), case(XNew,YNew,Plateau,b). % en haut à droite
+aVoisinCouleurOpposee(Plateau,X,Y,n):- coordDroite(X,Y,XNew,YNew), case(XNew,YNew,Plateau,b). % a droite
+aVoisinCouleurOpposee(Plateau,X,Y,n):- coordEnBasDroite(X,Y,XNew,YNew), case(XNew,YNew,Plateau,b). % en bas a droite
+aVoisinCouleurOpposee(Plateau,X,Y,n):- coordEnBas(X,Y,XNew,YNew), case(XNew,YNew,Plateau,b). % en dessous
+aVoisinCouleurOpposee(Plateau,X,Y,n):- coordEnBasGauche(X,Y,XNew,YNew), case(XNew,YNew,Plateau,b). % en bas a gauche
+aVoisinCouleurOpposee(Plateau,X,Y,n):- coordGauche(X,Y,XNew,YNew), case(XNew,YNew,Plateau,b). % a gauche
+aVoisinCouleurOpposee(Plateau,X,Y,n):- coordEnHautGauche(X,Y,XNew,YNew), case(XNew,YNew,Plateau,b). % en haut a gauche
+
+% vrai si une case avec un pion blanc a au moins un voisin noir
+aVoisinCouleurOpposee(Plateau,X,Y,b):- coordEnHaut(X,Y,XNew,YNew), case(XNew,YNew,Plateau,n). % au dessus
+aVoisinCouleurOpposee(Plateau,X,Y,b):- coordEnHautDroite(X,Y,XNew,YNew), case(XNew,YNew,Plateau,n). % en haut à droite
+aVoisinCouleurOpposee(Plateau,X,Y,b):- coordDroite(X,Y,XNew,YNew), case(XNew,YNew,Plateau,n). % a droite
+aVoisinCouleurOpposee(Plateau,X,Y,b):- coordEnBasDroite(X,Y,XNew,YNew), case(XNew,YNew,Plateau,n). % en bas a droite
+aVoisinCouleurOpposee(Plateau,X,Y,b):- coordEnBas(X,Y,XNew,YNew), case(XNew,YNew,Plateau,n). % en dessous
+aVoisinCouleurOpposee(Plateau,X,Y,b):- coordEnBasGauche(X,Y,XNew,YNew), case(XNew,YNew,Plateau,n). % en bas a gauche
+aVoisinCouleurOpposee(Plateau,X,Y,b):- coordGauche(X,Y,XNew,YNew), case(XNew,YNew,Plateau,n). % a gauche
+aVoisinCouleurOpposee(Plateau,X,Y,b):- coordEnHautGauche(X,Y,XNew,YNew), case(XNew,YNew,Plateau,n). % en haut a gauche
+
+% calcule les coordonnees X2 et Y2
+% permet d avancer dans une direction
+coordEnHaut(X,Y,X2,Y2):- X2 is X-1, Y2 is Y.
+coordEnHautDroite(X,Y,X2,Y2):- X2 is X-1, Y2 is Y+1.
+coordDroite(X,Y,X2,Y2):- X2 is X, Y2 is Y+1.
+coordEnBasDroite(X,Y,X2,Y2):- X2 is X+1, Y2 is Y+1.
+coordEnBas(X,Y,X2,Y2):- X2 is X+1, Y2 is Y.
+coordEnBasGauche(X,Y,X2,Y2):- X2 is X+1, Y2 is Y-1.
+coordGauche(X,Y,X2,Y2):- X2 is X, Y2 is Y-1.
+coordEnHautGauche(X,Y,X2,Y2):- X2 is X-1, Y2 is Y-1.
+
+
+listeCoupsPossibles(Plateau,Couleur,ListeCoupsPossibles):-
+    between(0,7,X), 
+    between(0,7,Y), 
+    coupPossible(Plateau,X,Y,Couleur),
