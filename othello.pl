717a258f9341c525e8c81496a88f36ba5b4da078
cmenseau <cyrielle.menseau@insa-lyon.fr>
2019-10-24 09:24:05 +0200

    othello joueur contre ia ok

--- othello.pl
+++ othello.pl
@@ -7,7 +7,11 @@ play :-
     read(PlayerType),
     
     (PlayerType = j -> 
-        write('Joueur vs Ordi \n')
+        write('Joueur vs Ordi \n'),
+	write('Choix de l\'heuristique : (1, 2, 3, 4)\n'),
+        read(HeuristicIndex),
+        write('Type de l\'heuristique de l\'ordinateur : '), write(HeuristicIndex), 
+        write('\n')
         ; (PlayerType = o ->
             write('Choix de l\'heuristique : (1, 2, 3, 4)\n'),
             read(HeuristicIndex),
@@ -31,16 +35,25 @@ roundLoop(Plateau, Color, PlayerType, HeuristicIndex, PreviousPlayed) :-
         (PlayerType = j -> 
             entrerCoup(X, Y),
             placerPion(Plateau, Color, X, Y, PlateauFinal)
-            ; (PlayerType = o ->
+            ; PlayerType = o ->
                 choixCoupPossible(Plateau, Color, CoupsPossible, HeuristicIndex, PlateauFinal)
-                ; fail)
+                ; PlayerType = oj ->
+		    choixCoupPossible(Plateau, Color, CoupsPossible, HeuristicIndex, PlateauFinal)
+			; fail
         ),      
         writeln('Fin Manche - Joué.'),
         print_matrice(PlateauFinal),
-        roundLoop(PlateauFinal, NewColor, PlayerType, HeuristicIndex, true)
+	passerJoueurSuivant(PlayerType, NextPlayerType),
+        roundLoop(PlateauFinal, NewColor, NextPlayerType, HeuristicIndex, true)
     
     ; PreviousPlayed = true -> 
         write('Fin Manche - Non Joué.\n'),
-        roundLoop(Plateau, NewColor, PlayerType, HeuristicIndex, false)        
-        ; afficherResultat(Plateau)
+	passerJoueurSuivant(PlayerType, NextPlayerType),
+        roundLoop(Plateau, NewColor, NextPlayerType, HeuristicIndex, false)        
+        
+	; afficherResultat(Plateau)
     ).
+
+passerJoueurSuivant(j,oj).
+passerJoueurSuivant(oj,j).
+passerJoueurSuivant(o,o).
