f13e38416548373179ce7d436504ec016da658cb
cmenseau <cyrielle.menseau@insa-lyon.fr>
2019-10-24 10:22:50 +0200

    ordi vs ordi avec 2 heuristiques différentes okay

--- othello.pl
+++ othello.pl
@@ -11,18 +11,25 @@ play :-
 	write('Choix de l\'heuristique : (1, 2, 3, 4)\n'),
         read(HeuristicIndex),
         write('Type de l\'heuristique de l\'ordinateur : '), write(HeuristicIndex), 
-        write('\n')
-        ; (PlayerType = o ->
-            write('Choix de l\'heuristique : (1, 2, 3, 4)\n'),
-            read(HeuristicIndex),
-            write('Ordi vs Ordi : '), write(HeuristicIndex), 
-            write('\n')
-            ; fail)),
-            
-    print_matrice(Plateau),
-    write('Before Round Loop\n'),
-    roundLoop(Plateau, n, PlayerType, HeuristicIndex, true).
-    
+        write('\n'),
+	print_matrice(Plateau),
+	write('Before Round Loop\n'),
+	roundLoop(Plateau, n, PlayerType, HeuristicIndex, true)
+        
+	; (PlayerType = o ->
+            write('Choix de l\'heuristique des noirs : (1, 2, 3, 4)\n'),
+            read(HeuristicIndex1),
+	    write('Choix de l\'heuristique des blancs : (1, 2, 3, 4)\n'),
+            read(HeuristicIndex2),
+            write('Ordi (noir) vs Ordi (blanc) : '), 
+	    write(HeuristicIndex1),write(' vs '), write(HeuristicIndex2),
+            write('\n'),
+	    print_matrice(Plateau),
+    	    write('Before Round Loop\n'),
+	    roundLoop(Plateau, n, PlayerType, HeuristicIndex1, HeuristicIndex2, true)
+		; fail)).
+
+% appelé pour joueur vs joueur (1 IA = 1 heuristique)
 roundLoop(Plateau, Color, PlayerType, HeuristicIndex, PreviousPlayed) :-
     write('Début Manche : '), write(PlayerType), write(" "), writeln(Color),
 
@@ -35,9 +42,7 @@ roundLoop(Plateau, Color, PlayerType, HeuristicIndex, PreviousPlayed) :-
         (PlayerType = j -> 
             entrerCoup(X, Y),
             placerPion(Plateau, Color, X, Y, PlateauFinal)
-            ; PlayerType = o ->
-                choixCoupPossible(Plateau, Color, CoupsPossible, HeuristicIndex, PlateauFinal)
-                ; PlayerType = oj ->
+            ; PlayerType = oj ->
 		    choixCoupPossible(Plateau, Color, CoupsPossible, HeuristicIndex, PlateauFinal)
 			; fail
         ),      
@@ -54,6 +59,29 @@ roundLoop(Plateau, Color, PlayerType, HeuristicIndex, PreviousPlayed) :-
 	; afficherResultat(Plateau)
     ).
 
+% appelé pour pc vs pc (2 IA = 2 heuristiques)
+roundLoop(Plateau, Color, PlayerType, HeuristicIndexCurrent, HeuristicIndex2, PreviousPlayed) :-
+    write('Début Manche : '), write(PlayerType), write(" "), writeln(Color),
+    write('Heuristique appliquée :'), writeln(HeuristicIndexCurrent),
+
+    reverseCouleur(Color, NewColor),
+    listeCoupsPossibles(Plateau, Color, CoupsPossible),
+    write('Coup Possible : '),
+    writeln(CoupsPossible),
+
+    (listeNonVide(CoupsPossible) ->        
+        choixCoupPossible(Plateau, Color, CoupsPossible, HeuristicIndexCurrent, PlateauFinal),
+        writeln('Fin Manche - Joué.'),
+        print_matrice(PlateauFinal),
+        roundLoop(PlateauFinal, NewColor, NextPlayerType, HeuristicIndex2, HeuristicIndexCurrent, true)
+    
+    ; PreviousPlayed = true -> 
+        write('Fin Manche - Non Joué.\n'),
+        roundLoop(Plateau, NewColor, NextPlayerType, HeuristicIndex2, HeuristicIndexCurrent, false)        
+        
+	; afficherResultat(Plateau)
+    ).
+
+
 passerJoueurSuivant(j,oj).
 passerJoueurSuivant(oj,j).
-passerJoueurSuivant(o,o).
